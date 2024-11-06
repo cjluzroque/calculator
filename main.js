8d373ed95153cb49b070b1e3b828a43f035706df
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2024-11-06 01:42:37 -0500

    Add event listener and logic to add in input values and clear if needed

--- main.js
+++ main.js
@@ -1,6 +1,73 @@
 
 
 
+const button = document.querySelector('#create');
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const current = document.querySelector('#current');
+
+const numbers = "1234567890";
+const operators = "+-/*";
+
+let valStack = []; // Hold operands 
+let calcStack = []; // Hold operators 
+let operandCount = 0;
+let digits = 0;
+
+const calc = document.querySelector('.calc');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        console.log(button.textContent);
+
+        if (numbers.includes(button.textContent)) {
+            valStack.push(button.textContent);
+            digits++;
+            console.log(valStack);
+            console.log(calcStack);
+
+            input.textContent = input.textContent + '' + button.textContent;
+        }
+        if (operators.includes(button.textContent)) {
+            calcStack.push(button.textContent);
+            console.log(valStack);
+            let operand = valStack.join('');
+            valStack.splice(operandCount, digits);
+            digits = 0;
+            valStack.push(operand);
+            operandCount++;
+            console.log(valStack);
+            console.log(calcStack);
+        }
+        if (button.textContent == "AC") {
+            valStack = [];
+            calcStack = [];
+            operandCount = 0;
+            digits = 0;
+            console.log(valStack);
+            console.log(calcStack);
+        }
+        if (button.textContent == "C") {
+            valStack.pop();
+            calcStack.pop();
+            operandCount--;
+            digits = 0;
+            console.log(valStack);
+            console.log(calcStack);
+        }
+
+
+
+
+
+
+
+    });
+});
+
+
+
 // Addition function
 function add(a, b) {
 	return(a + b);
