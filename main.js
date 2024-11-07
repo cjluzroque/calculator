dd0e841c2a9082ed6d4bcd23b29daef58fa2b708
“CJ” <1592105+cjluzroque@users.noreply.github.com>
2024-11-07 00:29:30 -0500

    Operate function is able to work with postfix expression

--- main.js
+++ main.js
@@ -7,11 +7,13 @@ const input = document.querySelector('input');
 const current = document.querySelector('#current');
 
 const numbers = "1234567890";
-const operators = "+-/*=";
+const operators = "+-/*";
 
 let valStack = []; // Hold operands 
 let newVal = []; // Hold value 
-let calcStack = []; // Hold operators 
+let stack = []; 
+let output = [12, 34, 56, 78, 90, '+', '+','+','+'];
+console.log(operate());
 
 const calc = document.querySelector('.calc');
 const buttons = document.querySelectorAll('button');
@@ -19,99 +21,95 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener("click", function () {
         console.log(button.textContent);
-
         if (numbers.includes(button.textContent)) {
             newVal.push(button.textContent);
-
-            input.textContent = input.textContent + '' + button.textContent;
-        }
+            console.log(newVal);
+            return;
+        } 
         if (operators.includes(button.textContent)) {
-            let operand = newVal.join('');
-            valStack.push(operand);
+            valStack.push(parseInt(newVal.join('')));
+            valStack.push(button.textContent);
             newVal = [];
-            if (button.textContent == "=") {
-                console.log("I made it");
-                let answer = valStack.reduce((a, b) => operate(a, b));
-                console.log(answer);
-                valStack = [];
-                newVal = [];
-                calcStack = [];
-                return;
-            }
-            calcStack.push(button.textContent);
             console.log(valStack);
-            console.log(calcStack);
+            return;
         }
         if (button.textContent == "AC") {
             valStack = [];
             newVal = [];
-            calcStack = [];
             console.log(valStack);
-            console.log(calcStack);
+            return;
         }
         if (button.textContent == "C") {
             newVal = [];
             console.log(valStack);
-            console.log(calcStack);
+            console.log(newVal);
+            return;
         }
+        //When entering = it will push the last value into valStack
+        newVal.push(button.textContent);
+        valStack.push(parseInt(newVal.join('')));
+        console.log("Turn into postfix: " + valStack);
+
+        //Convert to postfix 
+        valStack.forEach(item => {
+            if (Number.isInteger(item)) {
+                output.push(item);
+            } else {
+              while ((stack != []) && (precedence(item) <= precedence(stack[-1]))) {
+                output.push(stack.pop());
+              } 
+              stack.push(item);
+            }
+        });
+        let items = stack.length;
+        for (i = 0; i < items; i++) output.push(stack.pop());
+        console.log(stack);
+        console.log(output);
+
+        //Calc postfix 
+        console.log(operate());
+        valStack = []; 
+        newVal = []; 
+        return;
     });
 });
 
-function operate(answer, current) {
-    let a = parseInt(answer);
-    let b = parseInt(current);
-    let currOperator = calcStack.pop();
-    if (currOperator == "+") return add(a, b);
-    if (currOperator == "-") return subtract(a, b);
-    if (currOperator == "/") return divide(a, b);
-    if (currOperator == "*") return multiply(a, b);
+
+//Calculate postfix
+function operate() {
+    let answer = []; 
+    output.forEach(item => { 
+        // If the scanned character is an operand, push it to the stack.
+        if (Number.isInteger(item)) {
+            answer.push(item);
+            console.log(answer);
+            return;
+        }
+        //  If the scanned character is an operator, pop two
+        // elements from stack apply the operator
+        let val1 = answer.pop();
+        let val2 = answer.pop();
+        switch(item) {
+            case '+':
+                answer.push(val2 + val1);
+                break;
+            case '-':
+                answer.push(val2 - val1);
+                break;
+            case '/':
+                answer.push(val2 / val1);
+                break;
+            case '*':
+                answer.push(val2 * val1);
+                break;
+        }
+    });
+    console.log("Answer: " + answer.slice('').pop());
+    return(answer.pop());
 }
 
-
-// Addition function
-function add(a, b) {
-    console.log(a + ' + ' + b + ' = ' + (a+b))
-	return(a + b);
-};
-
-// Subtraction function
-function subtract(a, b) {
-    console.log(a + ' - ' + b + ' = ' + (a-b))
-	return(a - b);
-};
-
-// Multiplication function
-function multiply(a, b) {
-    console.log(a + ' * ' + b + ' = ' + (a*b))
-	return(a * b);
-};
-
-// Division function
-function divide(a, b) {
-    console.log(a + ' / ' + b + ' = ' + (a/b))
-	return(a / b);
-};
-
-// Sum of Array 
-function addAll(a) {
-	return(a.reduce((a, b) => { add(a, b) }, 0));
-};
-
-// Multiply an Array 
-function multiplyAll(a) {
-  return(a.reduce((a, b) => { multiply(a, b) }, 1));
-};
-
-// Exponent 
-function power(a, b) {
-  return Math.pow(a, b);
-};
-
-// Factorial
-function fact(a) {
-	let answer = 1;
-  for (i = 0; i < a; i++) {
-    answer *= i+1;
-  }
-  return answer;
-};
+// Precedence 
+function precedence(operator) {
+    if((operator == "+") || (operator == "-")) return 11;
+    if((operator == "*") || (operator == "/")) return 12;
+}
